ac135cec5798994eb6c23fa51f32e6004c011718
Krzysztof Strugała <kr.strugala@gmail.com>
2018-10-20 19:49:26 +0200

    Another fixes

--- src/actions/diateryCompliance.js
+++ src/actions/diateryCompliance.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { GET_DIETARY_COMPLIANCE } from "../types";
 import api from "../api";
 
@@ -7,10 +8,15 @@ export const getDietaryCompliance = dietaryCompliance =>({
 });
 
 export const getDietaryComplianceInfo = (query) => dispatch =>
+{
+    axios.defaults.headers.common.Authorization = `Bearer ${  localStorage.getItem('tokenJWT')}`;
     api.dietaryCompliance.get(query).then(dietaryCompliance => dispatch(getDietaryCompliance(dietaryCompliance)));   
+}
 
 export const getDietaryComplianceInfoByDoctor = (patientId, query) => dispatch =>
+{
+    axios.defaults.headers.common.Authorization = `Bearer ${  localStorage.getItem('tokenJWT')}`;
     api.dietaryCompliance.getByDoctor(patientId, query).then(dietaryCompliance => dispatch(getDietaryCompliance(dietaryCompliance)));   
-
+}
 export const postDietaryCompliance = (payload) => dispatch => //eslint-disable-line
     api.dietaryCompliance.post(payload);   
